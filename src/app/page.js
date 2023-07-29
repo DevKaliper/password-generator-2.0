0e582c9e4890e0d8f0131d5d9393ad45bbb89951
Dev Kaliper <gxlthz@gmail.com>
2023-07-29 12:22:27 -0500

    ready

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+// app/page.tsx
+"use client"
+import {Button, ButtonGroup} from "@nextui-org/button";
+
+export default function App() {
+  return (
+    <Button>
+      Button
+    </Button>
+  );
+}
