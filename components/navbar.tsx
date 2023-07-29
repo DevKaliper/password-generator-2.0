ed24f91a5af6fad653bb4ad400ee2d021f5107ae
Dev Kaliper <gxlthz@gmail.com>
2023-07-29 18:28:45 -0500

    navbar done

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,8 +4,10 @@ import React from "react";
 import {Navbar as NextUiNavbar, NavbarBrand, NavbarContent, NavbarItem} from "@nextui-org/navbar";
 import {Button} from "@nextui-org/button";
 import {Link} from "@nextui-org/link";
+import {Chip} from "@nextui-org/chip";
 import BlackLogo from "./Logos/blackLogo";
 import WhiteLogo from "./Logos/whiteLogo";
+import { GithubLogo } from "./Logos/githubLogo";
 import {useTheme} from "next-themes";
 
 export const Navbar = () => {
@@ -17,32 +19,31 @@ export const Navbar = () => {
             theme === 'dark' ?  <WhiteLogo/> : <BlackLogo />
         }
         
-        <p className="font-bold text-inherit">ACME</p>
+        <p className="font-bold text-inherit">DevKaliper</p>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            v1.0.0
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page" color="success">
+            v2.0.0 <Chip color="success" variant="shadow" className="ml-1">Latest</Chip>
+          </Link>
+        </NavbarItem>
         <NavbarItem>
           <Link color="foreground" href="#">
             Features
           </Link>
         </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="#" aria-current="page">
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
-          </Link>
-        </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem>
+        
         <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
-            Sign Up
+          <Button as={Link} color="success" href="#" variant="flat" className="flex gap-2 justify-center font-bold dark:bg-gray-200 dark:text-black text-black items-center">
+            <GithubLogo />
+            GITHUB
           </Button>
         </NavbarItem>
       </NavbarContent>
